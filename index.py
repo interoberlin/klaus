19dcc2bfe78b6beeb20290f2cfa09a43a5487b2a
Interoberlin.de <thinktank@interoberlin.de>
2014-10-21 18:11:34 +0200

    private config moved to private.py

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python2.6
 
 TITLE = u"git.interoberlin.de"
-PATH_TO_REPOS = u"/home/git/" # end with "/"
+
+# make sure to end with "/"
+PATH_TO_REPOS = u"/home/git/" 
 
 # search for git repos in path_to_repos
 def isGit(path):
@@ -12,18 +14,20 @@ def isGit(path):
 from os import listdir
 REPOSITORIES = [PATH_TO_REPOS+repo for repo in filter(isGit, listdir(PATH_TO_REPOS))]
 
-# don't include blacklisted repos
-REPOSITORIES.remove('/home/git/tools.git')
-REPOSITORIES.remove('/home/git/cardsuilib.git')
+# don't include private repos
+from private import PRIVATE_REPOS
+for repo in PRIVATE_REPOS:
+	R = PATH_TO_REPOS+repo
+	if R in REPOSITORIES:
+		REPOSITORIES.remove(R)
 
 # klaus provides a make_app function that creates a WSGI application
 # Don't forget to add the path to klaus to WSGIPythonPath in your Apache config
 import sys
 sys.path.append('/usr/local/lib/python2.6/dist-packages')
-sys.path.append('/opt/klaus')
-sys.path.append('/opt/klaus/klaus')
-from klaus import make_app
+sys.path.append('/usr/local/lib/python2.6/dist-packages/klaus')
+sys.path.append('/usr/local/lib/python2.6/dist-packages/klaus/klaus')
 
 # Export WSGI application
+from klaus import make_app
 application = make_app(REPOSITORIES, TITLE)
-
